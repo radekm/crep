66c632a968c3a623df6cfe369f0f885cdf0390a1
Radek Micek <radek.micek@gmail.com>
2009-12-13 16:36:39 +0100

    Regular expression is instance of Show.

--- Core/Regex.hs
+++ Core/Regex.hs
@@ -1,4 +1,3 @@
-
 -- |
 -- Module    : Core.Regex
 -- Copyright : (c) Radek Micek 2009
@@ -9,6 +8,7 @@
 --
 module Core.Regex where
 
+import Data.List (intersperse)
 import Core.SymbSet
 
 -- |Represents extended regular expression.
@@ -17,10 +17,68 @@ data Regex = REpsilon
            | ROr [Regex]
            | RAnd [Regex]
            | RConcat [Regex]
-           | RNot Regex
            | RStar !Laziness Regex
            | RCounter !Laziness !Int !(Maybe Int) Regex
+           | RNot Regex
            | RGroup !Int Regex
 
 data Laziness = Greedy | Lazy
+              deriving Eq
+
+data Descriptor
+  = Epsilon | Atom | ONot | OCounter | OStar | OConcat | OAnd | OOr
+  deriving (Eq, Ord)
+
+data NaryOperator = OpConcat | OpAnd | OpOr
+
+instance Show Regex where
+  showsPrec _ = fst . shows'
+    where
+      -- Returns a pair @(repr, d)@ where @repr@ is a representation
+      -- of given regular expression and @d@ describes shape
+      -- of the regular expression.
+      shows' :: Regex -> (String -> String, Descriptor)
+      shows' REpsilon       = (id,        Epsilon)
+      shows' (RCharSet set) = (shows set, Atom)
+      shows' (ROr rs)       = showsNary OpOr     rs
+      shows' (RAnd rs)      = showsNary OpAnd    rs
+      shows' (RConcat rs)   = showsNary OpConcat rs
+      shows' (RStar Lazy r) = (wrap OStar r . ("*?" ++), OStar)
+      shows' (RStar _ r)    = (wrap OStar r . ('*':),    OStar)
+      shows' (RNot r)       = (('^':) . wrap ONot r,     ONot)
+      shows' (RGroup _ r)   = (('(':) . shows r . (')':), Atom)
+      shows' (RCounter lazy minRep maxRep r)
+        = (wrap OCounter r . (counterStr ++), OCounter)
+        where
+          counterStr = '{':minRepStr ++ maxRepStr ++ '}':lazyStr
+          lazyStr    = if lazy == Lazy then "?" else ""
+          minRepStr  = show minRep
+          maxRepStr  = case maxRep of Nothing -> ","
+                                      Just maxRep'
+                                        | minRep == maxRep' -> ""
+                                        | otherwise -> ',':show maxRep'
+      showsNary OpOr []     = shows' $ RCharSet empty
+      showsNary OpAnd []    = shows' $ RCharSet (complement empty)
+      showsNary OpConcat [] = shows' $ REpsilon
+      showsNary _ [r]      = shows' r
+      showsNary op rs
+        = ( foldl (.) id $ intersperse opRepr $ map (wrap opDesc) rs
+          , opDesc
+          )
+        where
+          opDesc = case op of { OpOr -> OOr 
+                              ; OpAnd -> OAnd ; OpConcat -> OConcat }
+          opRepr = case op of { OpOr     -> (" | " ++)
+                              ; OpAnd    -> (" & " ++) ; OpConcat -> id }
+      -- Returns representation of @r@ which can be given as an argument
+      -- to operator @parent@. That means represantation of @r@ is taken
+      -- and wrapped in parentheses if necessary.
+      wrap parent r
+        | (parent >= OConcat && parent >= child)
+            || (child == ONot && parent `elem` [OStar, OCounter])
+            || child == Atom
+          = repr
+        | otherwise = ("(?" ++) . repr . (')':)
+        where
+          (repr, child) = shows' r
 
