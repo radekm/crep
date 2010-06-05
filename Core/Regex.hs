e652fb8a41fd9f72a1690463e412156aad5c2fd2
Radek Micek <radek.micek@gmail.com>
2010-06-05 18:47:32 +0200

    Update of Regex. Capture is disallowed in Star, Repeat, Not.

--- Core/Regex.hs
+++ Core/Regex.hs
@@ -14,6 +14,8 @@ module Core.Regex
          Regex(..)
        , Yes
        , No
+       , removeCaptures
+       , listCaptures
        ) where
 
 -- | Type @'Regex' p s c@ represents regular expressions where
@@ -36,10 +38,10 @@ data Regex p s c where
   Concat :: Regex p s c -> Regex p s c -> Regex p s c
   -- @'Star' a@ denotes
   -- @L = {u_1 u_2 ... u_n | u_i in L(a), n is natural number}@.
-  Star :: Regex p s c -> Regex p s c
+  Star :: Regex p s No -> Regex p s c
   -- @'Repeat' lo hi a@ denotes
   -- @L = {u_1 ... u_n | u_i in L(a), n is natural number, lo <= n <= hi}@.
-  Repeat :: !Int -> !Int -> Regex p s c -> Regex p s c
+  Repeat :: !Int -> !Int -> Regex p s No -> Regex p s c
   -- @'Not' a@ denotes @L = all words except those in L(a)@.
   Not :: Regex p s No -> Regex p s c
   -- @'Capture' i a@ denotes @L = L(a)@.
@@ -50,3 +52,27 @@ data Yes
 
 -- | Phantom for 'Regex'.
 data No
+
+-- | Subexpressions matching @'Capture' _ r@ are replaced by @r@.
+removeCaptures :: Regex p s c -> Regex p s No
+removeCaptures Epsilon          = Epsilon
+removeCaptures (CharClass set)  = CharClass set
+removeCaptures (Or a b)         = removeCaptures a `Or` removeCaptures b
+removeCaptures (And a b)        = removeCaptures a `And` removeCaptures b
+removeCaptures (Concat a b)     = removeCaptures a `Concat` removeCaptures b
+removeCaptures (Star a)         = Star a
+removeCaptures (Repeat lo hi a) = Repeat lo hi a
+removeCaptures (Not a)          = Not a
+removeCaptures (Capture _ a)    = removeCaptures a
+
+-- | List of all @i@s from subexpressions matching @'Capture' i _@.
+listCaptures :: Regex p s c -> [Int]
+listCaptures Epsilon        = []
+listCaptures (CharClass _)  = []
+listCaptures (Or a b)       = listCaptures a ++ listCaptures b
+listCaptures (And a b)      = listCaptures a ++ listCaptures b
+listCaptures (Concat a b)   = listCaptures a ++ listCaptures b
+listCaptures (Star _)       = []
+listCaptures (Repeat _ _ _) = []
+listCaptures (Not _)        = []
+listCaptures (Capture i a)  = i:listCaptures a
